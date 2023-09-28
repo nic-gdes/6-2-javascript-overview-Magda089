7775016c6d6ee21d5a8cc58c3b88e7cdf24b81c3
Tasha Gerlitz <midzikowska@students.nic.edu>
2023-09-28 14:59:58 -0700

    Assignment from 1-5

--- javascript.js
+++ javascript.js
@@ -1,12 +1,34 @@
 // 1.1 Assignment: Where do you put JavaScript?
-
+    // Done
 // 1.2 Assignment: Variables
+var john_lastName = "Smith";
+var  john_age = "25";
+var john_country = "UnitedKingdom"
+
+console.log(john_lastName);
+console.log(john_age);
+console.log(john_country);
 
 // 1.3 Assignment: Variable Declaration & Initialization
+var a = "John;"
+var b = "Smith";
+var c = 25; 
+var d; false;
+var e;
+
+
+console.log (a,b,c,d,e);
 
 // 1.4 Assignment: Complete your statements
+x = 2; 
+y = 6;
+z = x + y;
 
 // 1.5 Assignment: Brackets, Braces and Parentheses
+var a = function(){
+    alert('Statement 1');
+   alert('Statement 2');
+}
 
 // 1.6 Assignment: Data Types
 
